d0a09c9fdedd79726f2088e609b0ab52ff18ab11
benlemus <benlemus234@gmail.com>
2024-05-22 17:03:44 -0700

    exercise done

---
+++ app.js
@@ -0,0 +1,38 @@
+// QUICK QUESTION 1
+new Set([1, 1, 2, 2, 3, 4]);
+//{1,2,3,4}
+
+// QUICK QUESTION 2
+[...new Set("referee")].join("");
+//'ref'
+
+// QUICK QUESTION 3
+let m = new Map();
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false);
+// 0: {Array(3) => true} 1: {Array(3) => false}
+
+// HAS DUPLICATE
+const hasDuplicate = (arr) => {
+  new Set(arr).size !== arr.length;
+};
+
+// VOWEL COUNT
+function isVowel(char) {
+  return "aeiou".includes(char);
+}
+
+function vowelCount(str) {
+  const vowelMap = new Map();
+  for (let char of str) {
+    let lowerCaseChar = char.toLowerCase();
+    if (isVowel(lowerCaseChar)) {
+      if (vowelMap.has(lowerCaseChar)) {
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      } else {
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
